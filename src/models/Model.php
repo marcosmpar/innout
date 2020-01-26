9d118289daf44d47e213882bbec0f9c20b0a6968
Marcos Paulo Alves Rodrigues <marcosmpar@gmail.com>
2020-01-26 08:51:08 -0400

    config auth

--- src/models/Model.php
+++ src/models/Model.php
@@ -28,10 +28,9 @@ class Model {
     public static function getOne($filters = [], $columns = '*') {
         $class = get_called_class();
         $result = static::getResultSetFromSelect($filters, $columns);
-
         return $result ? new $class($result->fetch_assoc()) : null;
     }
-    
+
     public static function get($filters = [], $columns = '*') {
         $objects = [''];
         $result = static::getResultSetFromSelect($filters, $columns);
